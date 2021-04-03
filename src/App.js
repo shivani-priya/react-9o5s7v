8b3553e7a49ddf28cb2052cd3d85eeec1db35fe7
shivani-priya <shivani.priya762@gmail.com>
2021-04-03 02:32:27 -0700

    3 files modified

--- src/App.js
+++ src/App.js
@@ -47,6 +47,7 @@ export default function App() {
                   ".png"
                 }
               />
+              <HomePage />
             </div>
             <HomePage />
           </Route>
--- src/routes/HomePage.js
+++ src/routes/HomePage.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 export default function HomePage() {
+  
   return <div>Home Page Details</div>;
 }
