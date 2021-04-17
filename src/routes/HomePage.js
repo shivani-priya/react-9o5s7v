73b7976d49ca988557adffcac5169133fc7a467b
shivani-priya <shivani.priya762@gmail.com>
2021-04-17 12:21:10 -0700

    1 file modified

--- src/routes/HomePage.js
+++ src/routes/HomePage.js
@@ -1,6 +1,21 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-export default function HomePage() {
-  
+import axios from "axios";
+import { randomId } from "./App";
+
+export default function HomePage(props) {
+  const [pokemons, setPokemons] = useState([]);
+  // axios.get("https://pokeapi.co/api/v2/pokemon/1").then(result => {
+  //   console.log(result.data);
+  // });
+  fetch("https://pokeapi.co/api/v2/pokemon/1", {
+    // configuration
+  })
+    .then(response => response.json())
+    .then(response => {
+      // do something with data
+      console.log(response.name);
+      console.log(props.randomId);
+    });
   return <div>Home Page Details</div>;
 }
